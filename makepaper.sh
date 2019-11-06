eca44c8035a92b36e259f591d21481b52e22ad03
Sean Gallagher <seangllghr@gmail.com>
2019-11-06 02:28:21 -0500

    Rewrote makepaper.sh. Basically from scratch.

--- makepaper.sh
+++ makepaper.sh
@@ -1,72 +1,125 @@
 #!/bin/bash
 
-build_launcher () {
+init_document () {
+    document_filetype=$(echo $1 | cut -d'.' -f 2)
+    if [ $document_filetype == "md" ] || [ $document_filetype == "txt" ]; then
+        document=$1
+    elif test -f "$1.md" || test -f "$1.txt"; then
+        if test -f "$1.md"; then
+            document="$1.md"
+        else
+            document="$1.txt"
+        fi
+    else
+        echo "Document does not appear to be a Markdown source file."
+        return 1
+    fi
+}
+
+init_buildtype () {
     case $1 in
+        "default")
+            init_buildtype "apa-html"
+            ;;
         "apa-html")
-            node $makepaperdir/apa-html.js --installRoot=$makepaperdir $2
+            buildtype="apa-html"
+            ;;
+        "math-latex")
+            buildtype="math-latex"
             ;;
         *)
-            echo 'Error: specified build type not found.'
+            echo "$1 is not a valid build type."
+            return 1
+            ;;
+    esac
+}
+
+load_makepaper_config () {
+    if test -f .makepaper; then
+        source .makepaper
+        echo "Loaded settings from .makepaper."
+    else
+        echo "Loaded default build settings."
+        return 1
+    fi
+}
+
+load_settings () {
+    str=$(readlink -f $(which makepaper))
+    makepaperdir=${str%/*}
+    if [ $# -eq 0 ]; then
+        load_makepaper_config
+    elif [ $# -eq 1 ]; then
+        if [ $1 == "start" ]; then
+            echo "Starting watcher..."
+            load_makepaper_config
+            watcher=true
+        elif ! init_buildtype $1 > /dev/null; then
+            load_makepaper_config
+            if ! init_document $1 > /dev/null; then
+                echo "Warning: Not a valid buildtype or document. Falling back."
+            fi
+        fi
+    elif [ $# -eq 2 ]; then
+        if [ $1 == "start" ]; then
+            echo "Starting watcher..."
+            load_makepaper_config
+            watcher=true
+            if ! init_buildtype $2 > /dev/null; then
+                if ! init_document $2 > /dev/null; then
+                    echo "Warning: Not a valid build type or document. Falling back."
+                fi
+            fi
+        else
+            if ! init_buildtype $1 > /dev/null; then
+                if ! init_buildtype $2 > /dev/null; then
+                    echo "Warning: No valid build type provided. Falling back."
+                fi
+            fi
+            if ! init_document $1 > /dev/null; then
+                if ! init_document $2 > /dev/null; then
+                    echo "Warning: No valid document provided. Falling back."
+                fi
+            fi
+        fi
+    else
+        echo "Error: Too many arguments"
+        exit 1
+    fi
+}
+
+build_launcher () {
+    case $buildtype in
+        "apa-html")
+            node $makepaperdir/apa-html.js --installRoot=$makepaperdir $document
+            ;;
+        "math-latex")
+            echo "Building with Pandoc/LaTeX"
+            pandoc \
+                --template=$makepaperdir/templates/math.tex \
+                --to=latex \
+                --output=$(echo $document | cut -d'.' -f 1).pdf \
+                $document
+            echo "Done!"
             ;;
     esac
 }
 
 build_watcher () {
-    inotifywait -e close_write,moved_to,create -m $2 |
+    inotifywait -e close_write,moved_to,create -m $document |
         while read -r directory events filename; do
-            build_launcher $1 $2
+            build_launcher
         done
 }
 
-str=$(readlink -f $(which makepaper))
-makepaperdir="${str%/*}"
+makepaperdir=""
+buildtype="apa-html"
+document="main.md"
+watcher=false
 
-if [ $# -eq 0 ]
-then
-    # Default build. Look for .makepaper file or build APA through HTML
-    if test -f "$cwd/.makepaper"
-    then
-        echo ".makepaper found, building with specified options."
-        # TODO: Parse .makepaper for build options.
-    else
-        echo "No .makepaper file found. Building with default options."
-        build_launcher 'apa-html' 'main.md'
-    fi
-elif [ $# -eq 1 ]
-then
-     if [ $(echo $1 | cut -d'.' -f 2) == 'md' ] && test -f $1
-     then
-         build_launcher 'apa-html' $1
-     elif [ $1 == 'start' ]
-     then
-         if test -f "$cwd/.makepaper"
-         then
-             echo "Configuration found. Starting watcher..."
-             # TODO: Parse the .makepaper for build options and launch the watcher
-         elif [ $# -gt 1 ]
-         then
-             echo "Error: live build must be preconfigured using .makepaper or launched with defaults."
-         else
-             echo "No configuration found; using defaults. Starting watcher..."
-             build_watcher 'apa-html' 'main.md'
-         fi
-     elif [ $1 == 'test' ]
-     then
-         node "$makepaperdir/test.js"
-     else
-         echo "Error: expected Markdown source or source file not found."
-     fi
-elif [ $# -eq 2 ]
-then
-    if [ $(echo $1 | cut -d'.' -f 2) == 'md' ]
-    then
-        build_launcher $2 $1
-    elif [ $(echo $2 | cut -d'.' -f 2) == 'md' ]
-    then
-        build_launcher $1 $2
-    else
-        echo "Error: Build type not found."
-    fi
+load_settings $@
+if $watcher; then
+    build_watcher
 else
-    echo "Error: too many arguments."
+    build_launcher
 fi
