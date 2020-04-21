b11cb6db1da223a0716b66bfd8a565ca5206e588
Sean Gallagher <seangllghr@gmail.com>
2020-04-20 21:14:30 -0400

    Arbitrary md includes, better cites/bib, radical refactoring

--- apa-html.js
+++ apa-html.js
@@ -4,6 +4,106 @@ const argv = require('minimist')(process.argv.slice(2))
 const { execSync } = require('child_process')
 const fs = require('fs-extra')
 const cheerio = require('cheerio')
+const matter = require('gray-matter')
+
+/**
+ * getExtension: Gets a file type extension from a filename, if there is one
+ *
+ * @param {string} path The path to the file
+ * @returns {string} the file type extension
+ */
+function getExtension(path) {
+    let basename = path.split(/[\\/]/).pop(),
+        pos = basename.lastIndexOf(".")
+
+    if (basename === "" || pos < 1)
+        return "";
+
+    return basename.slice(pos + 1)
+}
+
+/**
+ * include: Process a file for inclusion in the main document
+ *
+ * @param {string} filepath The path to the file to be included
+ * @return {string} the processed inclusion
+ */
+function include(filepath) {
+    try {
+        if (getExtension(filepath) == "html") {
+            return fs.readFileSync(filepath).toString()
+        } else if (getExtension(filepath) == "md") {
+            return processMarkdownInclusion(filepath)
+        } else {
+            console.log(`Warning: inclusion ${obj.html()} is of unknown type`)
+        }
+    }
+    catch (e) {
+        console.log(`Warning: file ${e.path} not found`)
+    }
+}
+
+/**
+ * loadMain: load a Pandoc Markdown document into cheerio for further processing
+ *
+ * @param {string} sourceFile The path to the source file
+ * @param {string} installRoot The path to the makepaper install directory
+ * @return The processed cheerio DOM object of the main document
+ */
+function loadMain(sourceFile, installRoot) {
+    const pandocCommand = 'pandoc ' +
+          '-t html --standalone ' +
+          `--template=${installRoot}/templates/apa.html ` +
+          sourceFile
+    return cheerio.load(execSync(pandocCommand).toString())
+}
+
+/**
+ * loadMeta: load a Pandoc Markdown document and strip the YAML frontmatter
+ *
+ * @param {string} sourceFile The path to the file to load
+ * @returns A gray-matter file object with the document content and metadata
+ */
+function loadMeta(sourceFile) {
+    console.log('Loading metadata...')
+    try {
+        return matter.read(sourceFile)
+    }
+    catch (e) {
+        return matter.read(sourceFile, {delimiters: ['---', '...']})
+    }
+}
+
+/**
+ * processCitations: executes a final pass of Pandoc over the draft HTML file
+ *                   to process Pandoc-style citations
+ *
+ * @param {string} draftFile The path to the draft HTML file
+ * @param {string} installRoot The path to the makepaper install directory
+ * @param metaFile The gray-matter file object containing the file metadata
+ */
+function processCitations(draftFile, installRoot, metaFile) {
+    // TODO: Arbitrary bib files pulled from YAML header
+    let bibFile = 'refs.bib'
+    if (metaFile.data.hasOwnProperty('bibliography')) {
+        bibFile = metaFile.data.bibliography
+    }
+    pandocCommand = 'pandoc ' +
+        '--from=markdown ' + // target only the markdown citations
+        '--to=html ' +
+        `--output=${draftFile} ` +
+        '--filter=pandoc-citeproc ' +
+        `--csl=${installRoot}/styles/csl/apa.csl ` +
+        `--bibliography=${bibFile}`
+    execSync('echo $(cat ' + draftFile + ') | ' + pandocCommand)
+}
+
+function processMarkdownInclusion(inclusionPath) {
+    pandocCommand = 'pandoc ' +
+        '-t  html ' +
+        inclusionPath
+    return execSync(pandocCommand).toString()
+}
 
 console.log('This is the APA-HTML paper build script.')
 console.log(`Source file: ${argv._[0]}`)
@@ -11,28 +111,16 @@ console.log(`Source file: ${argv._[0]}`)
 const installRoot = argv.installRoot
 const sourceFile = argv._[0]
 
+const metaFile = loadMeta(sourceFile)
+console.log(metaFile.data)
+
 // Run Pandoc and load the generated HTML into Cheerio
 console.log('Generating HTML...')
-const pandocCommand = 'pandoc' +
-      ' --filter=pandoc-citeproc ' +
-      '-t html --standalone ' +
-      `--template=${installRoot}/templates/apa.html ` +
-      `--csl=${installRoot}/styles/csl/apa.csl ` +
-      `${sourceFile}`
-const $ = cheerio.load(execSync(pandocCommand).toString())
+const $ = loadMain(sourceFile, installRoot)
 
-/* Inclusions: Include arbitrary HTML files in the build document
- * TODO: It might be nice to someday support arbitrary Markdown, also.
- */
 console.log('Sourcing inclusions...')
 $('.include').each(function (i, obj) {
-    try {
-        let inclusion = fs.readFileSync($(this).html()).toString()
-        $(this).parent().replaceWith(inclusion)
-    }
-    catch (e) {
-        console.log(`Warning: file ${e.path} not found.`)
-    }
+    $(this).parent().replaceWith(include($(this).html()))
 })
 
 // Push figure classes up to the implicit figure element added by Pandoc
@@ -92,8 +180,10 @@ $('link[href="styles-apa.css"]').attr('href', styleLink)
 
 // Write out the HTML, typeset it with Prince, and clean up.
 console.log('Writing HTML...')
-fs.writeFileSync('draft.html', $.html())
+let draftFile = 'draft.html'
+fs.writeFileSync(draftFile, $.html())
+processCitations(draftFile, installRoot, metaFile)
 console.log('Generating PDF...')
 execSync('prince draft.html', {stdio: 'inherit'})
-fs.unlinkSync('draft.html')
+// fs.unlinkSync('draft.html')
 console.log('Done!')
